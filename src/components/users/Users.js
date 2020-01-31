40be2fe775972da27d028dbf9ebacbc76f32a8bb
s7jovananukic <jovana.nukic@s7designcreative.com>
2020-01-31 08:46:02 +0100

    User & Single user

---
+++ src/components/users/Users.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import UserItem from "./UserItem";
+
+class Users extends Component {
+    state= {
+        users: [
+            {
+                login: "mojombo",
+                id: 1,
+                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+                html_url: "https://github.com/mojombo"
+            },
+            {
+                login: "defunkt",
+                id: 2,
+                avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+                html_url: "https://github.com/defunkt"
+            }
+        ]
+    }
+    render() {
+        return (
+            <div style={useStyle}>
+                {this.state.users.map(user => (
+                    <UserItem key={user.id} user={user}/>
+                ))}
+            </div>
+        )
+    }
+}
+
+const useStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3,1fr)",
+    gridGap: "1rem"
+}
+
+export default Users
